b65d9053042939ea3451e2b316fb2eb6fe602cdb
emarti2 <eva.martin@bsc.es>
2023-01-19 17:02:35 +0100

    index breakpoints

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,28 @@
 <template>
-    <v-container>
-      <h4 class="text-h5 text-center mt-10 title">Welcome to the FAIRification Tool Discoverer</h4>
+    <v-container 
+        :fluid="$vuetify.breakpoint.mdAndDown"
+        :id="$vuetify.breakpoint.xl ? 'discoverer-large' : ''">
+        <h4 class="text-h5 text-center mt-10 title">Welcome to the FAIRification Tool Discoverer</h4>
       <div class="text-body-2 mt-5 mb-10 text-center">
         Discovering tools for your FAIRification tasks
       </div>
 
       <v-row 
-        justify="space-around">
+        justify='space-around'
+        >
         <v-col
-          md="3"
+          cols="10"
+          sm="8"
+          md="4"
+          lg="4"
+          xl="4"
           v-for="item in cards">
           <v-card
-            class="mx-auto pt-3"
-            max-width="344"
-            
+            class="mx-auto pt-3 pl-2 pr-2"
+            :max-width="cardMaxWidth"
+            :min-height="cardMinHeight"
             >
-            <v-card-title class="justify-center">
+            <v-card-title class="justify-space-around">
               <v-icon class="display-2" >{{item.icon}}</v-icon>
             </v-card-title>
             <v-card-subtitle class="text-center  pt-3">
@@ -36,9 +43,7 @@
             </v-card-actions>
           </v-card>
           </v-col>
-      
       </v-row>
-
   </v-container>
 </template>
 <style scoped>
@@ -46,9 +51,11 @@
 	color: #3949AB !important;
 }
 
+#discoverer-large {
+  width: 70%;
+}
 .v-card{
   height: 280px;
-  width: 300px;
   margin: 10px;
 }
 
@@ -61,15 +68,29 @@
 </style>
 
 <script>
-import { mapGetters } from 'vuex';
 
 export default {
     name: "index",
     computed: {
-        ...mapGetters({
-            
-          })
+        cardMaxWidth() {
+            switch (this.$vuetify.breakpoint.name) {
+            case 'xs': return 400
+            case 'sm': return 400
+            case 'md': return 300
+            case 'lg': return 350
+            case 'xl': return 350
+            }
         },
+        cardMinHeight() {
+            switch (this.$vuetify.breakpoint.name) {
+            case 'xs': return 220
+            case 'sm': return 300
+            case 'md': return 250
+            case 'lg': return 250
+            case 'xl': return 250
+            }
+        }
+    },
     created() {
         
     },
