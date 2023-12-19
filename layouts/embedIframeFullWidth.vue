4acaa0abf1625a5a3f737e7e0c73666e9b6f3faa
Eva Martin del Pico <eva.mdpico@gmail.com>
2023-12-19 13:02:40 +0100

    feat: api page

---
+++ layouts/embedIframeFullWidth.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-app>
+      <HeaderMenu />
+      <v-main> 
+        <Nuxt />
+      </v-main>
+    </v-app>
+  </template>
+  <script>
+  import HeaderMenu from '../components/Header/HeaderMenu.vue'
+
+  export default {
+    name: 'embedIframeFullWidth',
+    components: {
+        HeaderMenu
+    }
+  }
+
+</script>
---
+++ pages/api.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-container fluid class="main-container">
+            <iframe
+            width="100%" 
+            height="110%" 
+            frameborder="0"
+            src="http://localhost:8000/docs"
+            scrolling="yes"
+            ></iframe>
+
+    </v-container>
+</template>
+<script>
+
+export default {
+    name: 'api',
+    layout: 'embedIframeFullWidth'
+}
+</script>
+
+<style scoped>
+
+
+.main-container{
+    height: 100vh;
+    width: 100vw;
+    padding: 0;
+    margin: 0;
+}
+
+
+</style>
+
